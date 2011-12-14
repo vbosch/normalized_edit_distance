55cccb23f1acee558ea7beab168c749ed45edb51
vbosch <vbosch@gmail.com>
2011-12-14 13:31:30 +0100

    added frontier selection functionality

--- lib/normalized_edit_distance/line_description_file.rb
+++ lib/normalized_edit_distance/line_description_file.rb
@@ -4,10 +4,11 @@ module NormalizedEditDistance
 
     attr_reader :lines ,:frontiers
 
-    def initialize(file_name)
+    def initialize(file_name,frontiers_to_consider)
       @file_name= file_name
       @lines = Array.new
       @frontiers = Array.new
+      @frontiers2consider = frontiers_to_consider
     end
 
     def read
@@ -18,13 +19,21 @@ module NormalizedEditDistance
       end
     end
 
+    def process_initial_frontier?
+      @frontiers2consider == :initial or @frontiers2consider == :both
+    end
+
+    def process_last_frontier?
+          @frontiers2consider == :last or @frontiers2consider == :both
+    end
+
     def process_line(line)
       values = line.split
       if values[0]=="Line"
         @lines.push values[2..3].map{|val| val.to_i}
 
-        @frontiers.push lines.last[0] #if lines.last[0] != @frontiers.last
-        @frontiers.push lines.last[1]
+        @frontiers.push lines.last[0] if process_initial_frontier?
+        @frontiers.push lines.last[1] if process_last_frontier?
       end
     end
 
